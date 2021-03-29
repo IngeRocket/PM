857b23bb7d6051c497702d0d89d8ec83e27a4a23
Julio Mendoza <47239389+IngeRocket@users.noreply.github.com>
2021-03-29 17:02:38 -0600

    php update 1

--- php/conexion.php
+++ php/conexion.php
@@ -3,7 +3,7 @@
 		$databasehost = "127.0.0.1";	
 		$databaseuser = "root";
 		$databasepass = "root";
-		$databasename = "BDM_PROYECTO";
+		$databasename = "PM_PROYECTO";
 
 		$mysqli = new mysqli($databasehost, $databaseuser, $databasepass, $databasename,3306);
 		if ($mysqli->connect_errno) {
--- php/webservice.php
+++ php/webservice.php
@@ -10,6 +10,11 @@
 			$email = $_POST['email'];
 			RegistroUsuario($user, $pass, $email);
 		}
+		case "Login":{
+			$user = $_POST['user'];
+			$pass = $_POST['pass'];
+			RegistroUsuario($user, $pass);
+		}
 		default:
 			{
 				alert("Hola");
@@ -18,7 +23,34 @@
 
 
 	function RegistroUsuario(usuario, password, email){
-		
+		$sentencia = $conexion->prepare("CALL SP_Registro(?,?,?)");
+		$sentencia->bind_param('sss', usuario, email, password);
+		$sentencia->execute();
+
+		$resultado = $sentencia->get_result();
+		while( $r = $resultado->fetch_assoc()) {
+		                $rows[] = $r;
+		         }                    
+		echo json_encode($rows,JSON_UNESCAPED_UNICODE);     
+
+		$sentencia->close();
+		$conexion->close();
+	}
+
+
+	function Login(usuario, password){
+		$sentencia = $conexion->prepare("CALL SP_Login(?,?)");
+		$sentencia->bind_param('ss', usuario, password);
+		$sentencia->execute();
+
+		$resultado = $sentencia->get_result();
+		while( $r = $resultado->fetch_assoc()) {
+		                $rows[] = $r;
+		         }                    
+		echo json_encode($rows,JSON_UNESCAPED_UNICODE);     
+
+		$sentencia->close();
+		$conexion->close();
 	}
 
 ?>
