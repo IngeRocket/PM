741fcf3ffcdd77424952a8f97bc3d9277dc4b8b5
Julio Mendoza <47239389+IngeRocket@users.noreply.github.com>
2021-03-31 15:22:51 -0600

    Index update
    Js redireccionamiento
    validacion de sesion

---
+++ js/sesion.js
@@ -0,0 +1,38 @@
+var Usuario = "";
+var idUsuario = 0;
+
+$(document).ready(	function(){
+	/*alert("Hola");*/
+		$("body").on("click",".cuenta-opc1", function(){
+			if(Usuario==""){
+				window.location.href="login.html";	
+			}else{
+				//opciones de cuenta como cambiar password
+			}	
+		});
+
+		$("body").on("click",".cuenta-opc2", function(){
+			if(Usuario==""){
+				window.location.href="Registro.html";	
+			}else{
+				//opciones de cuenta como cambiar password
+			}	
+		});
+
+		$("body").on("click",".logo", function(){
+				window.location.href="index.html";					
+		});
+
+});
+
+function Requisitos(){
+ /* Revisar si existe la variable sesion y si tiene valor con local storage*/
+var sesion = localStorage.getItem("sesion");
+
+		if (sesion==null){
+		sesion = false;
+		}else{
+
+		}
+}
+
