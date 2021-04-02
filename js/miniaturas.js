c605a5528eb4caa653c2ade02d37c4e8ea9ec064
Julio Mendoza <47239389+IngeRocket@users.noreply.github.com>
2021-04-01 19:57:41 -0600

    Funciones Miniaturas
    Funciones y arreglo a enlaces en index

--- js/miniaturas.js
+++ js/miniaturas.js
@@ -1,4 +1,21 @@
 function Miniatura(){
 	/* categoria > inferior > tarjetas > miniaturas*/
 	/* conforma div class imagen > img */
+	
+	
 }
+
+function AgregarPelicula(ruta){
+	$("#min-Peliculas").append("<div class='imagen'><a href='#'><img src='img/Peliculas/1.jpg'></a></div>");
+}
+function AgregarSerie(ruta){
+	$("#min-Series").append("<div class='imagen'><a href='#'><img src='img/Series/1.jpg'></a></div>");
+}
+function AgregarJuego(ruta){
+	$("#min-Juegos").append("<div class='imagen'><a href='#'><img src='img/Juegos/1.jpg'></a></div>");
+}
+function AgregarPrograma(ruta){
+	$("#min-Programas").append("<div class='imagen'><a href='#'><img src='img/Programas/1.jpg'></a></div>");
+}
+
+/* al cargar resulltados de la base de datos, se llena automaticamente cada contenedor de elementos */
