43e01a8835749ef06fb32fb6bace480abf9c2df8
Julio Mendoza <47239389+IngeRocket@users.noreply.github.com>
2021-04-02 22:47:13 -0600

    Update Datos default.sql

--- MYSQL/Datos default.sql
+++ MYSQL/Datos default.sql
@@ -23,7 +23,7 @@ insert into Imagen(img_ruta,img_formato)values('img/programas/3.jpg','JPG');
 insert into Imagen(img_ruta,img_formato)values('img/programas/4.jpg','JPG');
 insert into Imagen(img_ruta,img_formato)values('img/programas/5.jpg','JPG');
 insert into Imagen(img_ruta,img_formato)values('img/programas/6.jpg','JPG');
-*/
+
 describe serie;
 insert into serie(s_titulo, s_descripcion,s_temporada,s_year,s_peso) values('The Flash','23/23 episodios 1080p',1,'2014','2.4 GB');
 insert into serie(s_titulo, s_descripcion,s_temporada,s_year,s_peso) values('Brooklyn Nine-Nine','24/24 episodios 1080p',1,'2013','4.5 GB');
@@ -39,4 +39,17 @@ insert into imagen_serie(is_id_s,is_id_img,is_portada)values(5,10,1);
 
 select * from imagen;
 select * from serie;
-select * from imagen_Serie;
+select * from imagen_Serie; 
+*/
+/*		VISTAS		*/
+/* HACER LO MISMO CON LA CATEGORIA DE PELICULAS, PROGRAMAS Y JUEGOS*/
+DELIMITER //
+create view V_Series as
+select S.s_id ID, S.s_titulo Serie, S.s_year Estreno, S.s_fsubida Subida, I.img_ruta Portada, S.s_peso Peso, S.s_visita Visitas from Imagen as I
+join imagen_serie as IMGS
+on IMGS.is_id_img = I.img_id
+join Serie as S
+on S.s_id = IMGS.is_id_s;
+//
+
+SELECT * FROM V_SERIES;
