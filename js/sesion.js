bc570e340b9868304168f0ed1b852d61c6d88aea
IngeRocket <angel.66.mendoza@gmail.com>
2021-04-09 13:10:29 -0500

    Cambios
    cambios en formulario de sesion
    +falta revisar el de registro

--- js/sesion.js
+++ js/sesion.js
@@ -3,25 +3,35 @@ var idUsuario = 0;
 var invitado = false;
 
 $(document).ready(	function(){
-	/*alert("Hola");*/
+
+		var invitado = localStorage.getItem("Invitado");		
+		
 		$("body").on("click",".cuenta-opc1", function(){
 			if(Usuario==""){
-				window.location.href="login.html";	
+				IrLogIn();	
 			}else{
 				//opciones de cuenta como cambiar password
+				/* al cambiar, si da clic me voy a configuracion*/
 			}	
 		});
 
 		$("body").on("click",".cuenta-opc2", function(){
 			if(Usuario==""){
-				window.location.href="Registro.html";	
+				IrRegistro();	
 			}else{
 				//opciones de cuenta como cambiar password
+				/* al cambiar, esta opcion sera de log out */
 			}	
 		});
 
 		$("body").on("click",".logo", function(){
-				window.location.href="index.html";					
+				IrPrincipal();					
+		});
+		$("body").on("click","#btn-login", function(){
+			//funcion de login					
+		});
+		$("body").on("click","#btn-registro", function(){					
+			IrRegistro();
 		});
 
 });
@@ -31,9 +41,28 @@ function Requisitos(){
 var sesion = localStorage.getItem("sesion");
 
 		if (sesion==null){
-		sesion = false;
-		}else{
+			sesion = false;
+			
+			}else{
 
 		}
 }
 
+function NavegacionGuess(valor){
+	if(valor){
+		localStorage.setItem("Invitado",true);
+		window.location.href="index.html";
+	}else{
+		localStorage.setItem("Invitado",false);
+		window.location.href="index.html";
+	}
+}
+function IrRegistro(){
+	window.location.href="registro.html";
+}
+function IrLogIn(){
+	window.location.href="login.html";	
+}
+function IrPrincipal(){
+	window.location.href="principal.html";	
+}
