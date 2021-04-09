cb289b6747f09d28052e3b7c51148dab64fbceea
IngeRocket <angel.66.mendoza@gmail.com>
2021-04-08 19:28:57 -0500

    Resultados categoria

--- js/vistas.js
+++ js/vistas.js
@@ -1,3 +1,34 @@
+	$(document).ready(	function(){
+		$("body").on("click","#Buscar",function(){
+			var dataToSend = { 
+				action: "Catalogo"
+				};
+			$.ajax({
+			url: "php/webservice.php",
+			async: true,
+			type: 'POST',
+			data: dataToSend, 
+			success: function (data){
+
+					var datos = JSON.parse(data);			
+					for (var i = 0; i < datos.length; i++) {
+
+						if(datos[i].Pelicula != null)
+							AgregarPelicula(datos[i].ID,datos[i].Ruta,datos[i].Peso);
+						else
+							if(datos[i].Serie != null)
+								AgregarSerie(datos[i].ID,datos[i].Ruta,datos[i].Peso);
+						
+					}  
+					//alert(data);
+					console.log(data);
+				}
+			});
+		});
+
+			
+	});	
+
 function AgregarPelicula(id,ruta,peso){
 			$(".resultados").append("<div class='resultado'>"+
 			"<div class='r-id'>"+id+"</div>"+
