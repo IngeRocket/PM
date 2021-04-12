715f5ff13d193193e85276cdbfd208062c88118d
Julio Mendoza <47239389+IngeRocket@users.noreply.github.com>
2021-04-11 20:45:18 -0500

    Update index.php

--- index.php
+++ index.php
@@ -5,11 +5,12 @@
 <script type="text/javascript" src="libs/jquery-2.1.4.min.js"></script>
 <script type="text/javascript">
 	$(document).ready(function(){
-		window.location.href="index.html";
+		//window.location.href="index.html";
+	alert("Hola");
 	});
 </script>	
 </head>
 <body>
-
+Hola
 </body>
 </html>
