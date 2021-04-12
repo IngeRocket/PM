211ef52407e163144f5fbea55ba073b64ff522c6
Julio Mendoza <47239389+IngeRocket@users.noreply.github.com>
2021-04-11 20:47:31 -0500

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 	<title>Index falso</title>
-<script type="text/javascript" src="libs/jquery-2.1.4.min.js"></script>
+<script type="text/javascript" src="js/jquery-2.1.4.min.js"></script>
 <script type="text/javascript">
 	$(document).ready( function(){
 		alert("Estoy vivo");	
