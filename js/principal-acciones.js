90239c2b918e29dc25d7ad65b99749d03ba4345b
Julio Mendoza <47239389+IngeRocket@users.noreply.github.com>
2021-04-17 15:52:45 -0500

    cambio de nombre

--- js/principal-acciones.js
+++ js/principal-acciones.js
@@ -1,3 +1,7 @@
+$(document).ready( function(){
+	alert("Hola");
+});
+
 function Opcion1(){
 //alert("Opcion 1");
 /*document.getElementById("nav-peliculas").style.background="rgba( 31,69,121,.3)";
@@ -29,3 +33,4 @@ document.getElementById("nav-series").style.background="none";
 document.getElementById("nav-juegos").style.background="none";
 document.getElementById("nav-programas").style.background="rgba( 31,69,121,.3)";*/
 }
+
