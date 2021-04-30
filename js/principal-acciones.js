66f6b90c50992729132cc887fee67de6104d2195
Julio Mendoza <47239389+IngeRocket@users.noreply.github.com>
2021-04-30 02:35:11 -0500

    Principal
    Redireccionamiento a elemento especifico desde pagina principal

--- js/principal-acciones.js
+++ js/principal-acciones.js
@@ -7,11 +7,20 @@ $(document).ready( function(){
 	document.getElementById("nav-peliculas").style="background-color: #2085b8;";
 	ContenidoPrincipal();
 
-	$("body").on("click", ".imagen", function(){
+	$("body").on("click", ".tarjetas .miniaturas .imagen", function(){
 		var identificador = $(this).attr("articulo");
 		var tipo = $(this).attr("tipo");
-		alert("Articulo: "+ identificador+" Tipo: "+tipo);
+		//alert("Articulo: "+ identificador+ " tipo "+tipo);
+		IrElementoEspecifico(identificador, tipo);
 	});
+
+	$("body").on("click", ".carousel-contenido .miniaturas .imagen .nada", function(){
+		var identificador = $(this).attr("articulo");
+		var tipo = $(this).attr("tipo");
+		//alert("Articulo: "+ identificador+ " tipo "+tipo);
+		IrElementoEspecifico(identificador, tipo);
+	});
+
 	$("body").on("click","#Buscar", function(){
 		//alert("Hola");
 				var titulobusqueda = document.getElementById("Texto-buscar").value;
@@ -25,44 +34,28 @@ function Opcion1(){
 	Seccion = "Peliculas";
 	Borrar();
 	document.getElementById("nav-peliculas").style="background-color: #2085b8;";
-	document.getElementById("C-1").src = Guardado[0].Ruta;
-	document.getElementById("C-2").src = Guardado[1].Ruta;
-	document.getElementById("C-3").src = Guardado[2].Ruta;
-	document.getElementById("C-4").src = Guardado[3].Ruta;
-	document.getElementById("C-5").src = Guardado[4].Ruta;
+	CargarAtributos(0,1);
 }
 
 function Opcion2(){
 	Seccion = "Series";
 	Borrar();
 	document.getElementById("nav-series").style="background-color: #2085b8;";
-	document.getElementById("C-1").src = Guardado[5].Ruta;
-	document.getElementById("C-2").src = Guardado[6].Ruta;
-	document.getElementById("C-3").src = Guardado[7].Ruta;
-	document.getElementById("C-4").src = Guardado[8].Ruta;
-	document.getElementById("C-5").src = Guardado[9].Ruta;
+	CargarAtributos(5,2);
 }
 
 function Opcion3(){
 	Seccion = "Juegos";
 	Borrar();
 	document.getElementById("nav-juegos").style="background-color: #2085b8;";
-	document.getElementById("C-1").src = Guardado[10].Ruta;
-	document.getElementById("C-2").src = Guardado[11].Ruta;
-	document.getElementById("C-3").src = Guardado[12].Ruta;
-	document.getElementById("C-4").src = Guardado[13].Ruta;
-	document.getElementById("C-5").src = Guardado[14].Ruta;
+	CargarAtributos(10,3);
 }
 
 function Opcion4(){
 	Seccion = "Programas";
 	Borrar();
 	document.getElementById("nav-programas").style="background-color: #2085b8;";
-	document.getElementById("C-1").src = Guardado[15].Ruta;
-	document.getElementById("C-2").src = Guardado[16].Ruta;
-	document.getElementById("C-3").src = Guardado[17].Ruta;
-	document.getElementById("C-4").src = Guardado[18].Ruta;
-	document.getElementById("C-5").src = Guardado[19].Ruta;
+	CargarAtributos(15,4);
 }
 
 function ContenidoPrincipal(){
@@ -118,17 +111,14 @@ function GrupoPrograma(articulo, titulo, ruta){
 }
 
 function CargaInfo(data){
+
 	var datos = JSON.parse(data);
 	console.log(data);
 	Guardado = JSON.parse(data);
 	
 	//carousel apartado peliculas (Siempre se muestra apartado peliculas al llegar a esta pagina)
-	document.getElementById("C-1").src = datos[0].Ruta;
-	document.getElementById("C-2").src = datos[1].Ruta;
-	document.getElementById("C-3").src = datos[2].Ruta;
-	document.getElementById("C-4").src = datos[3].Ruta;
-	document.getElementById("C-5").src = datos[4].Ruta; 
-/**/
+	CargarAtributos(0,1);
+
 	//Llenado Peliculas
 	for (var i = 20; i < 25; i++) {
 		GrupoPelicula(datos[i].ID, datos[i].Titulo, datos[i].Ruta);
@@ -150,4 +140,32 @@ function CargaInfo(data){
 	} 
 	/**/
 
+}
+
+function CargarAtributos(indice, tipo){
+	document.getElementById("C-1").src = Guardado[indice + 0].Ruta;
+	document.getElementById('C-1').setAttribute("articulo",Guardado[indice + 0].ID);
+	document.getElementById('C-1').setAttribute("tipo",tipo);
+
+	document.getElementById("C-2").src = Guardado[indice + 1].Ruta;
+	document.getElementById('C-2').setAttribute("articulo",Guardado[indice + 1].ID);
+	document.getElementById('C-2').setAttribute("tipo",tipo);
+
+	document.getElementById("C-3").src = Guardado[indice + 2].Ruta;
+	document.getElementById('C-3').setAttribute("articulo",Guardado[indice + 2].ID);
+	document.getElementById('C-3').setAttribute("tipo",tipo);
+
+	document.getElementById("C-4").src = Guardado[indice + 3].Ruta;
+	document.getElementById('C-4').setAttribute("articulo",Guardado[indice + 3].ID);
+	document.getElementById('C-4').setAttribute("tipo",tipo);
+
+	document.getElementById("C-5").src = Guardado[indice + 4].Ruta;
+	document.getElementById('C-5').setAttribute("articulo",Guardado[indice + 4].ID);
+	document.getElementById('C-5').setAttribute("tipo",tipo);
+}
+
+function IrElementoEspecifico(articulo, tipo){
+		localStorage.setItem("id-elemento",articulo);
+		localStorage.setItem("tipo-elemento",tipo);
+		window.location.href="elemento.html";
 }
